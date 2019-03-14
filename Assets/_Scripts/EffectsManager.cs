f8419a1adf88c438d5b603e9cbe68a77a6fe824b
johnhaysuper <johnhay@fandomcafe.com>
2019-03-14 13:56:40 +0800

    update trial select, trial 1 and trial 2

---
+++ Assets/_Scripts/EffectsManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EffectsManager : MonoBehaviour
+{
+    private static EffectsManager instance;
+
+    public static EffectsManager Instance { get { return instance; } }
+
+    //effects reference here
+    public List<GameObject> effects;
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+
+    public void ActivateEffect()
+    {
+
+    }
+	
+}
